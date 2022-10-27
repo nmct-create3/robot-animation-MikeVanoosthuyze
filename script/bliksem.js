46321b39ca1a009b8aeff10b4d117c59d65a9bea
Mike Vanoosthuyze <mike.vanoosthuyze@student.howest.be>
2022-10-27 16:58:56 +0200

    Klaar

---
+++ script/bliksem.js
@@ -0,0 +1,10 @@
+// de animatie starten
+// Maak van het animate element een variable en maak een functie die de de animatie start met de method beginElement.
+
+document
+  .querySelector(".js-button-lightning")
+  .addEventListener("click", function () {
+    console.log("clicked");
+    var animate = document.getElementById("lightning");
+    animate.beginElement();
+  });
